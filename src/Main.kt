6963238d8b62e7a361ad37819870610529f59ace
grieey <yangqing433520@163.com>
2020-10-22 16:46:27 +0800

    新增动态规划和滑动窗口的算法

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,9 @@
 /**
- * description: 联系算法的项目
+ * description: 练习算法的项目
  * @date: 2020/10/17 08:30
  * @author: Grieey
  */
 fun main(args: Array<String>) {
-  println("hello World")
+//  testMaxProfit()
+  testSlidingWindow()
 }
