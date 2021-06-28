705681f376a2b16be4e0a3cbce88a6015e25612f
grieey <yangqing433520@163.com>
2021-06-28 23:21:41 +0800

    新增去重数组算法

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let mut nums = vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+    let result = remove_duplicates(&mut nums);
+    println!("结果是{}", result);
+}
+
+pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+    match nums.len() {
+        0 => 0,
+        1 => 1,
+        n => {
+            let mut index = 0;
+            for i in 1..n {
+                if nums[index] != nums[i] {
+                    index += 1;
+                    nums[index] = nums[i];
+                }
+            }
+            (index + 1) as i32
+        }
+    }
 }
