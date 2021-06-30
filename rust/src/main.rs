684f9d0c1a37d6b511912422695e42ea3915b1a0
grieey <yangqing433520@163.com>
2021-06-30 22:34:09 +0800

    Update main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,6 +2,10 @@ fn main() {
     let mut nums = vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
     let result = remove_duplicates(&mut nums);
     println!("结果是{}", result);
+
+    let prices = vec![7, 1, 5, 3, 6, 4];
+    let sell = max_profit(&prices);
+    println!("结果是{}", sell);
 }
 
 pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
@@ -19,4 +23,28 @@ pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
             (index + 1) as i32
         }
     }
+}
+
+pub fn max_profit(prices: &Vec<i32>) -> i32 {
+    if prices.len() <= 1 {
+        return 0;
+    }
+
+    let mut buy = -prices[0];
+    let mut sell = 0;
+
+    for price in prices {
+        buy = max(buy, sell - price);
+        sell = max(sell, buy + price);
+    }
+
+    sell
+}
+
+pub fn max(num1: i32, num2: i32) -> i32 {
+    return if num1 > num2 {
+        num1
+    } else {
+        num2
+    };
 }
